8f545984ab3d861a65971d7b06c35a88d016b821
DavidGarciaCosta <david.garcia1@alumnos.uneatlantico.es>
2024-04-11 15:35:54 +0200

    Actualizacion archivo webController

--- ApiExpress-master/controllers/webController.js
+++ ApiExpress-master/controllers/webController.js
@@ -1,19 +1,23 @@
 const Menu = require('../modules/Menu');
 const Productos = require('../modules/Productos');
 const ProductosDeCantabria = require('../modules/ProductosDeCantabria');
+const Mesa = require('../modules/Mesa'); // Importación del esquema de mesas
 
-exports.getMenu = async(req, res)=>{
+exports.getMenu = async (req, res) => {
     try {
         const menus = await Menu.find();
-        res.render('menu', {menus});
+        res.render('menu', { menus });
     } catch (error) {
         console.log("Problemas al renderizar la pagina");
+        res.status(500).send("Error interno del servidor");
     }
 }
 exports.getMenuDelDia = async (req, res) => {
     try {
         const menuDelDia = await Productos.find({ esDelDia: true });
-        res.render('menu-Del-Dia', { menuDelDia });
+        const comida = menuDelDia.filter(producto => producto.tipo === 'comida');
+        const bebida = menuDelDia.filter(producto => producto.tipo === 'bebida');
+        res.render('menu-Del-Dia', { comida, bebida });
     } catch (error) {
         console.log("Problemas al renderizar la página del menú del día:", error);
         res.status(500).send("Error interno del servidor");
@@ -22,7 +26,9 @@ exports.getMenuDelDia = async (req, res) => {
 exports.getPlatoUnico = async (req, res) => {
     try {
         const platoUnico = await Productos.find({ esPlatoUnico: true });
-        res.render('plato-unico', { platoUnico });
+        const comida = platoUnico.filter(producto => producto.tipo === 'comida');
+        const bebida = platoUnico.filter(producto => producto.tipo === 'bebida');
+        res.render('plato-unico', { comida, bebida });
     } catch (error) {
         console.log("Problemas al renderizar la página de plato único:", error);
         res.status(500).send("Error interno del servidor");
@@ -32,7 +38,9 @@ exports.getPlatoUnico = async (req, res) => {
 exports.getMenuInfantil = async (req, res) => {
     try {
         const menuInfantil = await Productos.find({ esMenuInfantil: true });
-        res.render('menu-infantil', { menuInfantil });
+        const comida = menuInfantil.filter(producto => producto.tipo === 'comida');
+        const bebida = menuInfantil.filter(producto => producto.tipo === 'bebida');
+        res.render('menu-infantil', { comida, bebida });
     } catch (error) {
         console.log("Problemas al renderizar la página del menú infantil:", error);
         res.status(500).send("Error interno del servidor");
@@ -45,16 +53,58 @@ exports.getProductos = async (req, res) => {
         res.render('productos', { productos });
     } catch (error) {
         console.log("Problemas al renderizar la página de productos");
+        res.status(500).send("Error interno del servidor");
     }
 }
 exports.getProductosDeCantabria = async (req, res) => {
     try {
-        const productosdecantabria = await Productos.find({esCantabro : true});
+        const productosdecantabria = await Productos.find({ esCantabro: true });
         res.render('productosdecantabria', { productosdecantabria });
     } catch (error) {
         console.log("Problemas al renderizar la página de productos de Cantabria");
+        res.status(500).send("Error interno del servidor");
     }
 }
+
+exports.getProductosComida = async (req, res) => {
+    try {
+        const productosComida = await Productos.find({ tipo: 'comida' });
+        res.render('productosComida', { productosComida });
+    } catch (error) {
+        console.log("Problemas al renderizar la página de productos de comida:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+
+exports.getProductosBebida = async (req, res) => {
+    try {
+        const productosBebida = await Productos.find({ tipo: 'bebida' });
+        res.render('productosBebida', { productosBebida });
+    } catch (error) {
+        console.log("Problemas al renderizar la página de productos de bebida:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+
+exports.getMesas = async (req, res) => {
+    try {
+        const mesas = await Mesa.find();
+        res.render('mesas', { mesas });
+    } catch (error) {
+        console.log("Problemas al renderizar la página de mesas:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+
+exports.getInnovacion = (req, res) => {
+    try {
+        res.render('innovacion'); // Renderiza la vista de innovación
+    } catch (error) {
+        console.log("Problemas al renderizar la página de innovación:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+
 exports.home = (req, res) => {
     res.render('home');
 }
